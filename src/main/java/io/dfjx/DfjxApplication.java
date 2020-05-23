a6fb8f3cbf54b55d403d1afb514aab3515512674
mabuli <1220904008@qq.com>
2020-05-23 19:36:27 +0800

    maven默认使用dev配置

--- src/main/java/io/dfjx/DfjxApplication.java
+++ src/main/java/io/dfjx/DfjxApplication.java
@@ -11,12 +11,20 @@ import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 
 
 @SpringBootApplication(scanBasePackages = "io.dfjx")
 @MapperScan(basePackages = {"io.dfjx.module.*.dao"})
-public class DfjxApplication {
+public class DfjxApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        // 注意这里要指向原先用main方法执行的Application启动类
+        return builder.sources(DfjxApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(DfjxApplication.class, args);
